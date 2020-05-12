f67f7409836ee6f061d33ed8a898ca3fc3a05af3
Arlene <arlenejohn95@gmail.com>
2020-05-12 04:44:46 +0100

    3. Create Tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,32 @@
-pragma solidity^0.5.0;
+pragma solidity ^0.5.0;
 
 contract TodoList {
 	uint public taskCount = 0;
+
+	struct Task {
+		uint id;
+		string content;
+		bool completed;
+	}
+
+	mapping(uint => Task) public tasks;
+
+	event TaskCreated(
+    	uint id,
+    	string content,
+    	bool completed
+  	);
+
+	constructor() public{
+		createTask("check out EWH@UCD on instagram");
+	}
+
+	function createTask(string memory _content) public{
+		taskCount ++;
+		tasks[taskCount] = Task(taskCount,_content,false);
+		emit TaskCreated(taskCount, _content, false);
+
+	}
+
+
 }
